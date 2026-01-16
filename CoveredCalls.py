4e0ab57be2d9b8caeaab6192c23b45a093d98817
MomentumValue <32042972+dkeyes12@users.noreply.github.com>
2026-01-16 16:15:26 -0700

    Remove sensitive API keys and clean up code

--- CoveredCalls.py
+++ CoveredCalls.py
@@ -2,8 +2,8 @@
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import LimitOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-api_key = "PKK2RJBV3AN4KONDJSAU"
-secret_key = "OW19XLSUcjjBnyIVjFuz7TOEd4UvYVoQov23fjYb"
+api_key = ""
+secret_key = ""
 
 trading_client = TradingClient(api_key, secret_key, paper=True)
 
@@ -23,4 +23,5 @@ call_order_data = LimitOrderRequest(
     time_in_force=TimeInForce.DAY
 )
 call_order = trading_client.submit_order(order_data = call_order_data)
+
 trading_client.get_all_positions()
